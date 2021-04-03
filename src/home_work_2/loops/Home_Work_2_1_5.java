d938cf8e9a7963a92646b8d6a7520aa39c64fdac
Egor <7202928@mail.ru>
2021-04-03 21:31:57 +0300

    first commit

---
+++ src/home_work_2/loops/Home_Work_2_1_5.java
@@ -0,0 +1,17 @@
+package home_work_2.loops;
+
+public class Home_Work_2_1_5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i)
+                    + "\t" + "3 x " + i + " = " + (3 * i) + "\t" + "4 x " + i + " = " + (4 * i)
+                    + "\t" + "5 x " + i + " = " + (5 * i));
+        }
+        System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("6 x " + i + " = " + (6 * i)
+                    + "\t" + "7 x " + i + " = " + (7 * i) + "\t" + "8 x " + i + " = " + (8 * i)
+                    + "\t" + "9 x " + i + " = " + (9 * i));
+        }
+    }
+}
