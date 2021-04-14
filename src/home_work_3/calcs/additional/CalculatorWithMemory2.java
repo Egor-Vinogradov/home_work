ff2a4b0427d5aa5dfbeaddadc98472750e288eed
Egor <7202928@mail.ru>
2021-04-14 07:04:52 +0300

    first commit

---
+++ src/home_work_3/calcs/additional/CalculatorWithMemory2.java
@@ -0,0 +1,92 @@
+package home_work_3.calcs.additional;
+
+import home_work_3.calcs.api.ICalculator;
+
+public class CalculatorWithMemory2 implements ICalculator {
+    private ICalculator calculator;
+    private long count;
+    // переменная "память"
+    private double memory;
+
+    double rezult = 0;
+
+    /**
+     * Конструктор принимает значение выбранного вида калькулятора
+     * @param calculator возвращает выбранный калькулятор для дальнейшей работы
+     */
+    public CalculatorWithMemory2(ICalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    /**
+     * Функция возвращает значение "памяти"
+     * @return возвращаемое значение
+     */
+    public double getMemory() {
+        return memory;
+    }
+
+    /**
+     * Функция фиксирует в память операцию, вызванную после определенного метода
+     */
+    public void setMemoryLastOperation() {
+        memory = rezult;
+    }
+
+    @Override
+    public double addition(double a, double b) {
+        count++;
+        rezult = calculator.addition(a, b);
+        return rezult;
+    }
+
+    @Override
+    public double subtraction(double a, double b) {
+        count++;
+        rezult = calculator.subtraction(a, b);
+        return rezult;
+    }
+
+    @Override
+    public double multiplication(double a, double b) {
+        count++;
+        rezult = calculator.multiplication(a, b);
+        return rezult;
+    }
+
+    @Override
+    public double division(double a, double b) {
+        count++;
+        rezult = calculator.division(a, b);
+        return rezult;
+    }
+
+    @Override
+    public double pow(double value, int powValue) {
+        count++;
+        rezult = calculator.pow(value, powValue);
+        return rezult;
+    }
+
+    @Override
+    public double absNumber(double a) {
+        count++;
+        rezult = calculator.absNumber(a);
+        return rezult;
+    }
+
+    @Override
+    public double sqrt(double a) {
+        count++;
+        rezult = calculator.sqrt(a);
+        return rezult;
+    }
+
+    /**
+     * Возвращаем значение счетчика при использовании класса
+     * @return возвращает значение счетчика
+     */
+    public long getCountOperation() {
+        return count;
+    }
+}
--- src/home_work_3/runners/CalculatorWithMemoryMain.java
+++ src/home_work_3/runners/CalculatorWithMemoryMain.java
@@ -1,6 +1,7 @@
 package home_work_3.runners;
 
 import home_work_3.calcs.additional.CalculatorWithMemory;
+import home_work_3.calcs.additional.CalculatorWithMemory2;
 import home_work_3.calcs.simple.CalculatorWithOperator;
 
 public class CalculatorWithMemoryMain {
@@ -8,6 +9,12 @@ public class CalculatorWithMemoryMain {
         /*
         4.1 + 15 * 7 + (28 / 5) ^ 2
          */
+        versionOne();
+        versionTwo();
+
+    }
+
+    public static void versionOne() {
         double a = 4.1;
         double b = 15;
         double c = 7;
@@ -32,6 +39,32 @@ public class CalculatorWithMemoryMain {
         System.out.println("Значение в памяти: " + calc.getMemory());
 
         System.out.println("Количество операций: " + calc.getCountOperation());
+    }
 
+    public static void versionTwo() {
+        double a = 4.1;
+        double b = 15;
+        double c = 7;
+        double d = 28;
+        double e = 5;
+        int g = 2;
+
+        // для примера был взят CalculatorWithOperator
+        // если создать тип через интерфейс, тогда нужно добавлять команду для рассчета операций в интерфейс
+        CalculatorWithMemory2 calc = new CalculatorWithMemory2(new CalculatorWithOperator());
+//        CalculatorWithCounterAutoCompositeInterface calc = new CalculatorWithCounterAutoCompositeInterface(new CalculatorWithMathCopy());
+//        CalculatorWithCounterAutoCompositeInterface calc = new CalculatorWithCounterAutoCompositeInterface(new CalculatorWithMathExtends());
+
+        double rezult = calc.addition(calc.addition(a, calc.multiplication(b, c)),
+                calc.pow(calc.division(d, e), g));
+
+        System.out.println("4.1 + 15 * 7 + (28 / 5) ^ 2 = " + rezult);
+
+        // запись в память последней операции
+        calc.setMemoryLastOperation();
+        // получение значения из памяти
+        System.out.println("Значение в памяти: " + calc.getMemory());
+
+        System.out.println("Количество операций: " + calc.getCountOperation());
     }
 }
