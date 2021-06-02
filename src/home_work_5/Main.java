2f4c7d7a3361a060c51a987a6e858f7193a77bb4
Egor <7202928@mail.ru>
2021-06-02 14:22:56 +0300

    list commit

--- src/home_work_5/Main.java
+++ src/home_work_5/Main.java
@@ -4,17 +4,29 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] data1 = {1, 2, 3};
-//        Integer[] data1 = {1, null, 3, null, null, null};
-        // data = [1, 2, 3, null, null, null]
-        // data = [1, null, 3, null, null, null]
-        DataContainer<Integer> data = new DataContainer<>(data1);
-        data.add(1);
-        data.add(9);
-        data.add(8);
-        System.out.println(data.get(4));
-        System.out.println(data.get(5));
-        System.out.println(data.get(6));
-        System.out.println(data);
+
+        DataContainer<String> container = new DataContainer<>(); //Тут есть подвох в конструкторе, думайте пока сами
+        int index1 = container.add("Привет");
+        int index2 = container.add("Как дела");
+        int index3 = container.add("Работаю");
+        int index4 = container.add("Давай потом");
+        String text1 = container.get(index1);
+        String text2 = container.get(index2);
+        String text3 = container.get(index3);
+        String text4 = container.get(index4);
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);
+        container.delete(text1);
+        System.out.println(container.get(index1)); //Как дела
+
+        System.out.println("_________сортировка______________");
+
+        container.sort(null);
+
+        for (String s : container) {
+            System.out.println(s);
+        }
     }
 }
